fe66fb203b21808e153676c7a5d4647d8ac0719b
Swarit <boricpowderacid@gmail.com>
2025-11-03 12:47:52 +0530

    Added service worker

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+
+self.addEventListener("install", () => {
+  console.log("Service worker installed");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service worker activated");
+});
